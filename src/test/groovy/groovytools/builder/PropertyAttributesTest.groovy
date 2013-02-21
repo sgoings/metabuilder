a301ca84a617c907b52209481a026eb7cf68b207
damokles <damokles@8f5b34bb-663e-0410-9401-d0fa380fce8b>
2013-02-21 17:50:35 +0000

    Added new Testcase to reproduce bug that property attribute is not respected for nested properties
    git-svn-id: svn://svn.code.sf.net/p/groovytools/code/trunk/MetaBuilder/trunk@82 8f5b34bb-663e-0410-9401-d0fa380fce8b

---
+++ src/test/groovy/groovytools/builder/PropertyAttributesTest.groovy
@@ -0,0 +1,96 @@
+package groovytools.builder
+import spock.lang.Specification;
+import groovytools.builder.MetaBuilder
+
+/**
+ * Tests various attributes of properties.
+ *
+ * @author didge
+ * @version $Id: PropertyAttributesTest.groovy 73 2013-02-01 19:55:48Z damokles $
+ */
+class PropertyAttributesTest extends Specification {
+	
+	MetaBuilder mb = new MetaBuilder()
+
+	def setup() {
+		mb.define {
+			child(factory: 'TestChild') {
+				properties {
+					name()
+				}
+			}
+			parent(factory: 'TestParent') {
+				properties {
+					name()
+					name2(property: 'name', check: ~/Lists.*/)
+					name3(property: { b, v -> b.setName(v) } )
+					listOfChildren()
+					onlyChild(schema: 'child')
+					stepChild(property: 'onlyChild', schema: 'child')
+					otherChild(property: { b, v -> b.setOnlyChild(v) } , schema: 'child')
+				}
+			}
+		}
+	}
+	
+	def "Node name should be used as default"() {
+		when:
+		def parent = mb.build { parent(name: 'Lists of Children1') }
+		
+		then:
+		parent.name == 'Lists of Children1'
+	}
+	
+	def "Node name should be used as default for nested properties"() {
+		when:
+		def parent = mb.build { 
+			parent {
+				onlyChild(name: 'derp')
+			 }
+		}
+		then:
+		parent.onlyChild.name == 'derp'
+	}
+	
+	def "Property String attribute should be respected"() {
+		when:
+		def parent = mb.build { parent(name2: 'Lists of Children2') }
+		
+		then:
+		parent.name == 'Lists of Children2'
+	}
+	
+	
+	def "Property String attribute should be respected for nested properties"() {
+		when:
+		def parent = mb.build { 
+			parent {
+				stepChild(name: 'herp')
+			 }
+		}
+			
+		then:
+		parent.onlyChild.name == 'herp'
+	}
+	
+	def "Property Closure attribute should be respected"() {
+		when:
+		def parent = mb.build { parent(name3: 'Lists of Children2') }
+		
+		then:
+		parent.name == 'Lists of Children2'
+	}
+	
+	
+	def "Property Closure attribute should be respected for nested properties"() {
+		when:
+		def parent = mb.build {
+			parent {
+				otherChild(name: 'twerp')
+			 }
+		}
+			
+		then:
+		parent.onlyChild.name == 'twerp'
+	}
+}
